c5b2f643fc1e0c3274a536077aa971d997cf64dc
GxVcp <vieragabriel944@gmail.com>
2025-03-10 23:44:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Muestra un mensaje en la consola
+console.log("%c¡Hola desde JavaScript!", "color: #ff6600; font-size: 18px; font-weight: bold;");
+
+// Muestra un mensaje de alerta
+alert("%cBienvenido a la página web", "color: #ff6600; font-size: 16px; font-weight: bold;");
